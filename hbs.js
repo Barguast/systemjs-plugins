216889c7776a025424c8797bc14d52c6d4d5a799
Barguast <jamiewinder@gmail.com>
2015-01-17 23:06:27 +0000

    Adding hbs and hbss plugins

---
+++ hbs.js
@@ -0,0 +1,7 @@
+﻿"format cjs";
+var Handlebars = require("handlebars");
+
+exports.translate = function(load) {
+    var input = Handlebars.precompile(load.source);
+    return 'define(["handlebars"], function (Handlebars) { return Handlebars.template(' + input + '); });';
+}
---
+++ hbss.js
@@ -0,0 +1,14 @@
+﻿"format cjs";
+var $ = require("jquery");
+var Handlebars = require("handlebars");
+
+exports.translate = function (load) {
+    var arr = [];
+    $(load.source).filter("script[id][type='text/x-handlebars-template']").each(function (i, s) {
+        var template = Handlebars.precompile(s.innerHTML);
+        arr.push("'" + s.id + "':Handlebars.template(" + template + ")");
+    });
+    var hash = "{" + arr.join(",") + "}";
+    var out = 'define(["handlebars"], function (Handlebars) { return ' + hash + '; });';
+    return out;
+}
